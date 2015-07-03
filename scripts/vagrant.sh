1bd1e457cca86953b565b5973605cd32365e8ce7
蒼時弦也 <elct9620@frost.tw>
2015-07-03 20:42:22 +0800

    Fix vagrant ssh key path

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -4,10 +4,10 @@
 date > /etc/vagrant_box_build_time
 
 # Install Vagrant Keys
-mkdir -pm 700 /home/rails/.ssh
-wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O /home/rails/.ssh/authorized_keys
-chmod 0600 /home/rails/.ssh/authorized_keys
-chown -R rails /home/rails/.ssh
+mkdir -pm 700 /home/vagrant/.ssh
+wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O /home/vagrant/.ssh/authorized_keys
+chmod 0600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant /home/vagrant/.ssh
 
 # Customize message of the day
 echo 'Rails Development Box' > /etc/motd
